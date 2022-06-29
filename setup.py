bb5a1acda6c96cc8777e7d428a9631e557bdcd57
tructt <tructt@ftech.ai>
2022-06-30 01:45:52 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="TextToSpeech_Vietnamese",
+    version="0.0.1",
+    author="Tran Trung Truc",
+    author_email="tieuthu.anninh@gmail.com",
+    description="text to speech for vietnamese language",
+    long_description="Full description",
+    url="https://github.com/trungtruc123/TTS_Vietnamese",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
